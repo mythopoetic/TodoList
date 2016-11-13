d40a4c6fea1e309bbe4322f1aafe1691937eafba
mythopoetic <gustav.hjertman@gmail.com>
2016-11-13 16:01:18 +0100

    First commit

---
+++ src/TodoList/TodoList.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TodoList;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Gustav
+ */
+public class TodoList {
+
+    public static void main(String[] args) {
+        
+        ArrayList<String> todoList = new ArrayList<>();
+    }
+}
